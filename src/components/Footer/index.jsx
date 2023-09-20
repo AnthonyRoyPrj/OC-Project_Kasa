ec682980cd0db1eaea78a4124ce84d5c354ac662
Anthony Roy <anthonyroyuha@gmail.com>
2023-09-20 17:10:26 +0200

    Mise en forme du header et du footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,8 +1,33 @@
+import kasaLogoDark from "../../assets/logo-dark.svg"
+import styled from "styled-components"
+
+const FooterWrapper = styled.footer`
+    height: 144px;
+    padding-top: 66px;
+    background-color: #000000;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 30px;
+`
+
+const StyledText = styled.p`
+    color: #FFFFFF;
+    font-size: 24px;
+    font-weight: 500;
+    @media (max-width: 767px) {
+        font-size: 12px;
+    }
+`
+
 function Footer() {
     return (
-        <div>
-            Footer
-        </div>
+        <FooterWrapper>
+            <img src={kasaLogoDark} alt="Logo Kasa" />
+            <StyledText>
+                © 2020 Kasa. All rights reserved
+            </StyledText>
+        </FooterWrapper>
     )
 }
 
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,42 @@
+import kasaLogoLight from "../../assets/logo-light.svg"
 import { Link } from 'react-router-dom'
- 
+import styled from "styled-components"
+
+const StyledHeader = styled.header`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-top: 40px;
+    margin-bottom: 20px;
+`
+
+const StyledLogo = styled.img`
+    height: 68px;
+    @media (max-width: 767px) {
+        height: 47px;
+    }
+`
+
+const StyledNav = styled.nav`
+    display: flex;
+    gap: 60px;
+    font-size: 24px;
+    font-weight: 500;
+    @media (max-width: 767px) {
+        font-size: 12px;
+        gap: 12px;
+    }
+`
+
 function Header() {
     return (
-        <nav>
-            <Link to="/">Accueil</Link>
-            <Link to="/about">À Propos</Link>
-        </nav>
+        <StyledHeader>
+            <StyledLogo src={kasaLogoLight} alt="Logo Kasa"/>
+            <StyledNav>
+                <Link to="/">Accueil</Link>
+                <Link to="/about">À Propos</Link>
+            </StyledNav>
+        </StyledHeader>
     )
 }
 
