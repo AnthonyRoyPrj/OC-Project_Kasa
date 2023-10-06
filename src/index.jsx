a30f93986cc88a2afb6b12b5fcb7fecec24e56e3
Anthony Roy <anthonyroyuha@gmail.com>
2023-10-06 12:54:28 +0200

    Changement extension passant de js à jsx. Ajout d'un global style

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { createGlobalStyle } from "styled-components";
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -8,10 +9,30 @@ import About from './pages/About';
 import Lodging from './pages/Lodging';
 import Error from './pages/Error';
 
+const Globalstyle = createGlobalStyle`
+	* {
+		font-family: Montserrat;
+		margin: 0;
+		padding: 0;
+		color: #FF6060;
+	}
+	main, header {
+		margin: auto;
+		width: 88%;
+	}
+	a {
+		text-decoration: none;
+	}
+	ul {
+		list-style: none;
+	}
+`
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   	<React.StrictMode>
     	<Router>
+			<Globalstyle />
 			<Header />
 			<Routes>
 				<Route path="/" element={<Home />} />
