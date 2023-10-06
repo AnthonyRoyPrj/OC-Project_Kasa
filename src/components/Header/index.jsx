aa5da0393c9f6c05b41946cc9c974b51e8df5fe5
Anthony Roy <anthonyroyuha@gmail.com>
2023-10-06 13:01:23 +0200

    Adding underline link when on the current page + minor style changes

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,6 @@
 import kasaLogoLight from "../../assets/logo-light.svg"
 import { Link } from 'react-router-dom'
+import { useLocation } from "react-router-dom"
 import styled from "styled-components"
 
 const StyledHeader = styled.header`
@@ -8,11 +9,14 @@ const StyledHeader = styled.header`
     align-items: center;
     margin-top: 40px;
     margin-bottom: 20px;
+    @media (max-width: 768px) {
+        margin-top: 20px;
+    }
 `
 
 const StyledLogo = styled.img`
     height: 68px;
-    @media (max-width: 767px) {
+    @media (max-width: 768px) {
         height: 47px;
     }
 `
@@ -22,19 +26,34 @@ const StyledNav = styled.nav`
     gap: 60px;
     font-size: 24px;
     font-weight: 500;
-    @media (max-width: 767px) {
+    @media (max-width: 768px) {
         font-size: 12px;
         gap: 12px;
+        text-transform: uppercase;
     }
 `
 
+const StyledLink = styled(Link)`
+    ${({$activeLink}) => {
+        if ($activeLink === "active") {
+            return (
+                `text-decoration: underline;`
+        )} else {
+            return (
+                `text-decoration: none;`
+        )}
+    }
+}
+`
+
 function Header() {
+    const currentLocation = useLocation();
     return (
         <StyledHeader>
             <StyledLogo src={kasaLogoLight} alt="Logo Kasa"/>
             <StyledNav>
-                <Link to="/">Accueil</Link>
-                <Link to="/about">À Propos</Link>
+                <StyledLink to="/" $activeLink={currentLocation.pathname === "/" ? "active" : ""}>Accueil</StyledLink>
+                <StyledLink to="/about" $activeLink={currentLocation.pathname === "/about" ? "active" : ""}>À Propos</StyledLink>
             </StyledNav>
         </StyledHeader>
     )
