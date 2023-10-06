0355d14f5e0762c7b2cf31a5f5b3a39c1b824491
Anthony Roy <anthonyroyuha@gmail.com>
2023-10-06 12:58:50 +0200

    Error page

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -1,8 +1,56 @@
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+const ErrorWrapper = styled.main`
+    text-align: center;
+`
+
+const StyledErrorType = styled.h1`
+    margin-top: 130px;
+    margin-bottom: 40px;
+    font-size: 288px;
+    font-weight: 700;
+    @media (max-width: 767px) {
+        margin-bottom: 12px;
+        font-size: 99px;
+    }
+`
+
+const StyledErrorMessage = styled.p`
+    font-size: 36px;
+    font-weight: 500;
+    @media (max-width: 767px) {
+        font-size: 18px;
+    }
+`
+
+const StyledLink = styled(Link)`
+    display: block;
+    margin-top: 180px;
+    margin-bottom: 150px;
+    font-size: 18px;
+    font-weight: 500;
+    text-decoration: underline;
+    @media (max-width: 767px) {
+        margin-top: 130px;
+        margin-bottom: 200px;
+        font-size: 14px;
+    }
+`
+
 function Error() {
     return (
-        <div>
-            Erreur 404
-        </div>
+        <ErrorWrapper>
+            <StyledErrorType>
+                404
+            </StyledErrorType>
+            <StyledErrorMessage>
+                Oups! La page que vous demandez n'existe pas
+            </StyledErrorMessage>
+            <StyledLink to="/">
+                Retourner sur la page d'accueil
+            </StyledLink>
+        </ErrorWrapper>
     )
 }
 
